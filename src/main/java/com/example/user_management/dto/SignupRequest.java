c513fe01ca5691ac250a069d33d055eb0f7804cf
ashok ashok <manideepikapasarthi@gmail.com>
2026-03-28 22:54:50 +0530

    Added SignupRequest DTO with validation

---
+++ src/main/java/com/example/user_management/dto/SignupRequest.java
@@ -0,0 +1,19 @@
+package com.example.user_management.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+}
